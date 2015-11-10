71faa0345fe3e43d78a74b548aaaec426afdb687
Piotr Baniukiewicz <P.Baniukiewicz@warwick.ac.uk>
2015-11-10 13:07:14 +0000

    Moved external fiji starting script to 66 java

--- startTestFiji.sh
+++ startTestFiji.sh
@@ -1,10 +1,10 @@
 # This script starts Fiji application with debugger
 # Fiji and JRE have to exist beyond source tree
-JAVA_DIR="jdk1.8.0_51"
+JAVA_DIR="jdk1.8.0_66"
 RED='\033[1;31m'
 NC='\033[0m' # No Color
 echo -e ${RED}*********************************${NC}
 echo -e ${RED}* Started with java $JAVA_DIR *${NC}
 echo -e ${RED}*********************************${NC}
 echo
-Fiji.app/ImageJ-linux64 --debugger=8000 --java-home /home/baniuk/$JAVA_DIR
+~/Fiji.app/ImageJ-linux64 --debugger=8000 --java-home /home/baniuk/$JAVA_DIR
