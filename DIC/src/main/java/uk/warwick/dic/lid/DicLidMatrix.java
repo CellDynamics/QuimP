ccf0b0d467eef8d2f7b7c66b39b27eb71ae725e3
Piotr Baniukiewicz <P.Baniukiewicz@warwick.ac.uk>
2015-12-04 15:14:10 +0000

    #45 Added dependency of log4j and added initial projext for DIC matrix version

---
+++ DIC/src/main/java/uk/warwick/dic/lid/DicLidMatrix.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package uk.warwick.dic.lid;
+
+import ij.ImagePlus;
+import ij.process.ByteProcessor;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+/**
+ * Implementation of Kam algorithm with use of matrix approach
+ * @author baniuk
+ *
+ */
+public class DicLidMatrix {
+	
+//	final Logger logger = LoggerFactory.getLogger(DicLidMatrix.class);
+	protected static final Logger logger = LogManager.getLogger(DicLidMatrix.class.getName());
+	/**
+	 * Empty default constructor
+	 */
+	public DicLidMatrix() {
+
+	}
+	
+	/**
+	 * Reconstruct DIC image by LID method using matrix approach
+	 * Make copy of original image to not change it.
+	 * @param srcImage
+	 * @param s0
+	 * @return
+	 */
+	public ImagePlus DicReconstructionLidMatrix(ImagePlus srcImage, double s0) {
+		
+		logger.info("Height of image = "+String.valueOf(srcImage.getHeight()));
+		ImagePlus localCopy = srcImage.duplicate(); // make a copy of original
+		ByteProcessor tmpProcessor = (ByteProcessor) localCopy.getProcessor(); // get ByteProcessor to access pixels
+		
+		tmpProcessor.rotate(90);
+		
+		return localCopy; // return modified image
+	}
+
+}
---
+++ DIC/src/test/java/uk/warwick/dic/lid/DicLidMatrixTest.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ */
+package uk.warwick.dic.lid;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import ij.IJ;
+import ij.ImagePlus;
+
+/**
+ * @author baniuk
+ *
+ */
+public class DicLidMatrixTest extends DicLidMatrix {
+	
+	private ImagePlus image;
+	
+	public DicLidMatrixTest() {
+		
+	}
+
+	/**
+	 * Load test image
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		image = IJ.openImage("src/test/java/uk/warwick/dic/lid/testObject.tif"); // opens test image
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 * @warning May not detect changes done on image
+	 */
+	@After
+	public void tearDown() throws Exception {
+		if(image.changes) { // check if source was modified
+			image.changes = false; // set flag to false to prevent save dialog
+			image.close(); // close image
+			throw new Exception("Image has been modified"); // throw exception if source image was modified
+		}
+		image.close();
+	}
+
+	/**
+	 * @test Test method for {@link uk.warwick.dic.lid.DicLidMatrix#DicLidMatrix()}.
+	 */
+	@Test
+	public void testDicLidMatrix() {
+		// Empty constructor - nothing to do here
+	}
+
+	/**
+	 * @ test Test method for {@link uk.warwick.dic.lid.DicLidMatrix#DicReconstructionLidMatrix(ij.ImagePlus, double)}.
+	 * Saves output image at \c /tmp/testDicReconstructionLidMatrix.tif
+	 */
+	@Test
+	public void testDicReconstructionLidMatrix() {
+		ImagePlus ret;
+		ret = DicReconstructionLidMatrix(image,0);
+		IJ.saveAsTiff(ret, "/tmp/testDicReconstructionLidMatrix.tif"); 
+	}
+
+}
