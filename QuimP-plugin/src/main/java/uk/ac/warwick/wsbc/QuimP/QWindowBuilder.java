93db1f55aa3f9e47b02d67878ca3f1809353d7bf
Piotr Baniukiewicz <P.Baniukiewicz@warwick.ac.uk>
2016-01-29 14:39:26 +0000

    #62 Initial files

---
+++ QuimP-plugin/src/main/java/uk/ac/warwick/wsbc/QuimP/QWindowBuilder.java
@@ -0,0 +1,18 @@
+/**
+ * @file QWindowBuilder.java
+ * @date 29 Jan 2016
+ */
+package uk.ac.warwick.wsbc.QuimP;
+
+/**
+ * @author p.baniukiewicz
+ * @date 29 Jan 2016
+ *
+ */
+public abstract class QWindowBuilder {
+
+	public void BuildWindow(String def) {
+		
+	}
+
+}
---
+++ QuimP-plugin/src/test/java/uk/ac/warwick/wsbc/QuimP/QWindowBuilder_Test.java
@@ -0,0 +1,23 @@
+/**
+ * @file QWindowBuilder_Test.java
+ * @date 29 Jan 2016
+ */
+package uk.ac.warwick.wsbc.QuimP;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author p.baniukiewicz
+ * @date 29 Jan 2016
+ *
+ */
+public class QWindowBuilder_Test extends QWindowBuilder {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
