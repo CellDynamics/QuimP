995199c7f3b7ecbc9baa3b3d3e418f44db5a7541
Piotr Baniukiewicz <P.Baniukiewicz@warwick.ac.uk>
2016-03-17 13:22:44 +0000

    Generate doc commonly for all projects

--- generateDoc.sh
+++ generateDoc.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # This script generates Doxygen doc based on java source files
 # Outputs doxygen documentation using doxyfile avaiable at Doxygen_doc
+
 dot -Tpng Doxygen_doc/maven-structure.dot -o /tmp/maven-structure.png
 # copy only if changed to prevent pushing repo
 rsync -c /tmp/maven-structure.png Doxygen_doc/maven-structure.png
@@ -11,3 +12,5 @@ cd Doxygen_doc
 rm -rf html/*
 doxygen Doxyfile >/dev/null
 rsync -az -e 'ssh -p2222' --stats --delete Doxygen_doc html/ trac@trac-wsbc.linkpc.net:/var/www/restricted/Doxygen_doc/QuimP
+
+
