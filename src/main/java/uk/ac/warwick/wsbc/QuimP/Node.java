f456e5ea21395e065c7915fe60ed05343f785cf3
Piotr Baniukiewicz <P.Baniukiewicz@warwick.ac.uk>
2016-04-14 10:38:43 +0100

    Compiling example

--- src/main/java/uk/ac/warwick/wsbc/QuimP/Node.java
+++ src/main/java/uk/ac/warwick/wsbc/QuimP/Node.java
@@ -15,7 +15,7 @@ import uk.ac.warwick.wsbc.QuimP.geom.ExtendedVector2d;
  * @author rtyson
  *
  */
-public class Node extends Vertex {
+public class Node extends BidirectionalList<Node> {
     private ExtendedVector2d vel; // velocity of the nodes
     private ExtendedVector2d F_total; // total force at node
     private ExtendedVector2d prelimPoint; // point to move node to after all new
