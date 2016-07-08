c9bb84fc0d6f7f990e5f6aba4d7e219d41012d5e
Piotr Baniukiewicz <P.Baniukiewicz@warwick.ac.uk>
2016-07-08 12:56:46 +0100

    Fixed log in RW plugin

--- src/main/java/uk/ac/warwick/wsbc/QuimP/RandomWalkSegmentationPlugin_.java
+++ src/main/java/uk/ac/warwick/wsbc/QuimP/RandomWalkSegmentationPlugin_.java
@@ -10,6 +10,7 @@ import java.util.Map;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
 
 import ij.IJ;
 import ij.ImagePlus;
@@ -37,9 +38,11 @@ import uk.ac.warwick.wsbc.QuimP.plugin.randomwalk.RandomWalkSegmentation;
  *
  */
 public class RandomWalkSegmentationPlugin_ implements PlugIn {
-
     static {
-        System.setProperty("log4j.configurationFile", "qlog4j2.xml");
+        if (System.getProperty("quimp.debugLevel") == null)
+            Configurator.initialize(null, "log4j2_default.xml");
+        else
+            Configurator.initialize(null, System.getProperty("quimp.debugLevel"));
     }
     private static final Logger LOGGER =
             LogManager.getLogger(RandomWalkSegmentationPlugin_.class.getName());
