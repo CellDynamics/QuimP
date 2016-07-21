7368e0851f28c27c11c76cb1f16442821ed3dc0f
Piotr Baniukiewicz <P.Baniukiewicz@warwick.ac.uk>
2016-07-21 12:42:31 +0100

    changed generate doc to not create image

--- generateDoc.sh
+++ generateDoc.sh
@@ -25,12 +25,13 @@ else
 	UPLOAD=1
 fi
 
-dot -Tpng Doxygen_doc/maven-structure.dot -o /tmp/maven-structure.png
+# This image is now stored in Quimp.wiki repo
+# dot -Tpng Doxygen_doc/maven-structure.dot -o /tmp/maven-structure.png
 # copy only if changed to prevent pushing repo
-rsync -c /tmp/maven-structure.png Doxygen_doc/maven-structure.png
-mkdir -p Doxygen_doc/Doxygen_doc
-# for compatibility of paths between Readme.md and Readme.md embeded in Doxygen doc
-cp Doxygen_doc/maven-structure.png Doxygen_doc/Doxygen_doc/maven-structure.png
+#rsync -c /tmp/maven-structure.png Doxygen_doc/maven-structure.png
+#mkdir -p Doxygen_doc/Doxygen_doc
+## for compatibility of paths between Readme.md and Readme.md embeded in Doxygen doc
+#cp Doxygen_doc/maven-structure.png Doxygen_doc/Doxygen_doc/maven-structure.png
 cd Doxygen_doc
 rm -rf html/*
 CURRENT_DIR=$(pwd)
@@ -51,7 +52,8 @@ rm -rf /tmp/QuimP.wiki
 
 doxygen $DEFAULT_DOXYFILE >/dev/null
 if [ $UPLOAD -eq 1 ]; then
-	rsync -az -e 'ssh -p2222' --stats --delete Doxygen_doc html/ trac@trac-wsbc.linkpc.net:/var/www/restricted/Doxygen_doc/QuimP
+	# copy also images - all md files expect images in root of webpage (directory the doxegen was called from)
+	rsync -az -e 'ssh -p2222' --stats --delete *.png *.jpg html/ trac@trac-wsbc.linkpc.net:/var/www/restricted/Doxygen_doc/QuimP
 	rsync -lrtz -e "ssh -i ~/.ssh/pi -p 10222 -o 'IdentitiesOnly yes'" --delete --stats Doxygen_doc html/ pi@quimp.linkpc.net:/var/www/restricted/Doxygen_doc
 fi
 
