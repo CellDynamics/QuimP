a7e184b3ac03c304f621cd1fbd3a613f22fa2168
Piotr Baniukiewicz <P.Baniukiewicz@warwick.ac.uk>
2016-08-23 15:11:36 +0100

    Add line

--- src/main/java/uk/ac/warwick/wsbc/QuimP/plugin/Prot_Analysis/ProtrusionVis.java
+++ src/main/java/uk/ac/warwick/wsbc/QuimP/plugin/Prot_Analysis/ProtrusionVis.java
@@ -4,13 +4,26 @@
  */
 package uk.ac.warwick.wsbc.QuimP.plugin.Prot_Analysis;
 
+import java.awt.Color;
+import java.awt.Polygon;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import javax.vecmath.Point2d;
+import javax.vecmath.Point2i;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import ij.ImagePlus;
 import ij.gui.Overlay;
+import ij.gui.PointRoi;
+import ij.gui.PolygonRoi;
+import ij.gui.Roi;
+import uk.ac.warwick.wsbc.QuimP.GraphicsElements;
 import uk.ac.warwick.wsbc.QuimP.STmap;
 
 /**
@@ -19,13 +32,10 @@ import uk.ac.warwick.wsbc.QuimP.STmap;
  */
 public class ProtrusionVis {
     private static final Logger LOGGER = LogManager.getLogger(ProtrusionVis.class.getName());
-    private STmap mapCell;
-    private MaximaFinder mF;
-    ImagePlus originalImage;
+    private ImagePlus originalImage;
+    private Overlay overlay;
 
     public ProtrusionVis() {
-        mapCell = null;
-        mF = null;
         originalImage = null;
     }
 
@@ -33,21 +43,160 @@ public class ProtrusionVis {
      * @param qP 
      * 
      */
-    public ProtrusionVis(MaximaFinder mF, STmap mapCell) {
-        this.mapCell = mapCell;
-        this.mF = mF;
+    public ProtrusionVis(ImagePlus originalImage) {
+        this.originalImage = originalImage;
+        overlay = originalImage.getOverlay(); // check for existing overlay
+        if (overlay == null) // create new if no present
+            overlay = new Overlay();
 
     }
 
-    public void addPointsToImage() {
-        LOGGER.trace(originalImage);
+    /**
+     * Plot maxima found by {@link MaximaFinder} on current image.
+     * 
+     * @param mapCell 
+     * @param mF properly initialized {@link MaximaFinder} object.
+     */
+    public void addMaximaToImage(STmap mapCell, MaximaFinder mF) {
+
+        LOGGER.trace("Num of slices: " + originalImage.getStackSize());
         LOGGER.trace(mF.getMaxima());
         LOGGER.trace(mapCell);
-        Overlay overaly = new Overlay();
+        Polygon max = mF.getMaxima();
         double x[][] = mapCell.getxMap();
         double y[][] = mapCell.getyMap();
+        int[] indexes = max.xpoints;
+        int[] frames = max.ypoints;
 
-        LOGGER.trace(Arrays.toString(mapCell.getxMap()));
+        LOGGER.trace(Arrays.toString(frames));
+        for (int n = 0; n < max.npoints; n++) {
+
+            double xcoord = x[frames[n]][indexes[n]]; // screen coordinate of
+            double ycoord = y[frames[n]][indexes[n]]; // (frame,index) point
+            plotCircle(xcoord, ycoord, frames[n] + 1, Color.MAGENTA, 10);
+        }
+        originalImage.setOverlay(overlay); // add to image
+    }
+
+    /**
+     * 
+     * @param mapCell
+     * @param pL
+     */
+    public void addTrackingLinesToImage(STmap mapCell, List<PolygonRoi> pL) {
+        double x[][] = mapCell.getxMap();
+        double y[][] = mapCell.getyMap();
+        for (PolygonRoi pR : pL) {
+            List<Point2i> plR = PolygonRoi2Point2i(new ArrayList<PolygonRoi>(Arrays.asList(pR)));
+            Collections.sort(plR, new ListPoint2iComparator());
+            List<PolygonRoi> plRsorted = Point2i2PolygonRoi(plR); // again polygon but sorted along
+                                                                  // frames
+            float xcoord[] = new float[plRsorted.get(0).getNCoordinates()];
+            float ycoord[] = new float[plRsorted.get(0).getNCoordinates()];
+            for (int f = 0; f < plRsorted.get(0).getNCoordinates(); f++) {
+                xcoord[f] = (float) x[plRsorted.get(0).getPolygon().ypoints[f]][plRsorted.get(0)
+                        .getPolygon().xpoints[f]];
+                ycoord[f] = (float) y[plRsorted.get(0).getPolygon().ypoints[f]][plRsorted.get(0)
+                        .getPolygon().xpoints[f]];
+            }
+            for (int f = 0; f < plRsorted.get(0).getNCoordinates(); f++) {
+                PolygonRoi pRoi = new PolygonRoi(xcoord, ycoord, f + 1, Roi.POLYLINE);
+                pRoi.setPosition((int) plRsorted.get(0).getYCoordinates()[f] + 1);
+                pRoi.setStrokeColor(Color.GREEN);
+                pRoi.setFillColor(Color.GREEN);
+                overlay.add(pRoi);
+            }
+
+        }
+        // List<Point2i> plL = PolygonRoi2Point2i(pL); // convert to flat list of points
+        // // go through all frames and check whether there is any point to draw
+        // for (int f = 1; f <= originalImage.getStackSize(); f++) {
+        // final int c = f;
+        // List<Point2i> result =
+        // plL.stream().filter(e -> e.getX() == c).collect(Collectors.toList());
+        // if (result.isEmpty())
+        // continue;
+        // for (Point2i p : result) {
+        // double xcoord = x[p.x][p.y]; // screen coordinate of
+        // double ycoord = y[p.x][p.y]; // (frame,index) point
+        // plotPoint(xcoord, ycoord, p.y, Color.GREEN);
+        // }
+        //
+        // }
+        originalImage.setOverlay(overlay); // add to image
+    }
+
+    /**
+     * Plot filled circle on overlay.
+     * 
+     * @param x
+     * @param y
+     * @param frame
+     * @param color
+     * @param radius
+     */
+    private void plotCircle(double x, double y, int frame, Color color, double radius) {
+        // create ROI
+        PolygonRoi oR = new PolygonRoi(
+                GraphicsElements.plotCircle(new Point2d(x, y), (float) radius), Roi.POLYGON);
+        // set z-position of ROI!!!
+        oR.setPosition(frame);
+        oR.setStrokeColor(color);
+        oR.setFillColor(color);
+        overlay.add(oR); // add to collection of overlays
+    }
+
+    /**
+     * 
+     * @param x
+     * @param y
+     * @param frame
+     * @param color
+     */
+    private void plotPoint(double x, double y, int frame, Color color) {
+        PointRoi pR = new PointRoi(x, y);
+        pR.setPosition(frame);
+        pR.setStrokeColor(color);
+        pR.setFillColor(color);
+        overlay.add(pR); // add to collection of overlays
+    }
+
+    /**
+     * Convert list of Polygons to list of Points.
+     * <p>
+     * The difference is that for polygons points are kept in 1d arrays, whereas for Point2i they
+     * are as separate points that allows object comparison.
+     *  
+     * @param list List of polygons to convert
+     * @return List of points constructed from all polygons.
+     */
+    private List<Point2i> PolygonRoi2Point2i(List<PolygonRoi> list) {
+        List<Point2i> ret = new ArrayList<>();
+        for (PolygonRoi p : list) { // every polygon
+            Polygon pl = p.getPolygon();
+            for (int i = 0; i < pl.npoints; i++) // every point in it
+                ret.add(new Point2i(pl.xpoints[i], pl.ypoints[i]));
+        }
+        return ret;
+    }
+
+    /**
+     * Convert list of Points to list of Polygons.
+     * <p>
+     * The difference is that for polygons points are kept in 1d arrays, whereas for Point2i they
+     * are as separate points that allows object comparison.
+     *  
+     * @param list List of points to convert
+     * @return Polygon constructed from all points. This is 1-element list.
+     */
+    private List<PolygonRoi> Point2i2PolygonRoi(List<Point2i> list) {
+        List<PolygonRoi> ret = new ArrayList<>();
+        Polygon pl = new Polygon();
+        for (Point2i p : list) { // every point
+            pl.addPoint(p.getX(), p.getY());
+        }
+        ret.add(new PolygonRoi(pl, Roi.POLYLINE));
+        return ret;
 
     }
 
@@ -56,6 +205,26 @@ public class ProtrusionVis {
      */
     public ImagePlus getOriginalImage() {
         return originalImage;
+    }
+
+}
+
+/**
+ * Compare along frames (y-coordinate)
+ * 
+ * @author p.baniukiewicz
+ *
+ */
+class ListPoint2iComparator implements Comparator<Point2i> {
+
+    @Override
+    public int compare(Point2i o1, Point2i o2) {
+        if (o1.getY() < o2.getY())
+            return -1;
+        if (o1.getY() > o2.getY())
+            return 1;
+        else
+            return 0;
     }
 
 }
--- src/test/java/uk/ac/warwick/wsbc/QuimP/plugin/Prot_Analysis/ProtrusionVisTest.java
+++ src/test/java/uk/ac/warwick/wsbc/QuimP/plugin/Prot_Analysis/ProtrusionVisTest.java
@@ -4,7 +4,19 @@
  */
 package uk.ac.warwick.wsbc.QuimP.plugin.Prot_Analysis;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 import java.awt.Polygon;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.vecmath.Point2i;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -15,14 +27,15 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import ij.IJ;
+import ij.ImageJ;
 import ij.ImagePlus;
+import ij.gui.PolygonRoi;
+import ij.gui.Roi;
 import uk.ac.warwick.wsbc.QuimP.STmap;
 
 /**
@@ -37,14 +50,19 @@ public class ProtrusionVisTest {
         else
             Configurator.initialize(null, System.getProperty("quimp.debugLevel"));
     }
+
+    static Object accessPrivate(String name, ProtrusionVis obj, Object[] param,
+            Class<?>[] paramtype) throws NoSuchMethodException, SecurityException,
+            IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+        Method prv = obj.getClass().getDeclaredMethod(name, paramtype);
+        prv.setAccessible(true);
+        return prv.invoke(obj, param);
+    }
+
     private static final Logger LOGGER = LogManager.getLogger(ProtrusionVisTest.class.getName());
-    private static ImagePlus originalImage;
+    private ImagePlus originalImage;
     // http://stackoverflow.com/questions/16467685/difference-between-mock-and-injectmocks
-    @Mock
-    private MaximaFinder mF;
-    @Mock
-    private STmap mapCell;
-    @InjectMocks
+    // @InjectMocks
     private ProtrusionVis protrusionVis;
 
     /**
@@ -52,8 +70,6 @@ public class ProtrusionVisTest {
      */
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
-        originalImage = IJ.openImage(
-                "/home/baniuk/Documents/Kay-copy/KZ4/KZ4-220214-cAR1-GFP-devel5.5h-agar07-14.tif");
     }
 
     /**
@@ -70,6 +86,9 @@ public class ProtrusionVisTest {
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this); // must be as we create mocked classes in mocked already
                                             // QParams
+        originalImage = IJ.openImage(
+                "/home/baniuk/Documents/Kay-copy/KZ4/KZ4-220214-cAR1-GFP-devel5.5h-agar07-14.tif");
+        protrusionVis = new ProtrusionVis(originalImage);
     }
 
     /**
@@ -92,22 +111,92 @@ public class ProtrusionVisTest {
      */
     @Test
     public void testAddPointsToImage() throws Exception {
-        protrusionVis.originalImage = originalImage;
-        int[] frames = { 35, 28, 72, 72, 19, 140, 27 };
+        ImageJ ij = new ImageJ();
+        int[] frames = { 0, 1, 2, 3, 4, 5, 6 };
         int[] indexes = { 0, 0, 0, 0, 0, 0, 0 };
         double[][] xs = { { 10 }, { 50 }, { 100 }, { 150 }, { 200 }, { 300 }, { 400 } };
         double[][] ys = { { 50 }, { 60 }, { 160 }, { 210 }, { 360 }, { 460 }, { 510 } };
+        STmap mapCell = Mockito.mock(STmap.class);
+        MaximaFinder mF = Mockito.mock(MaximaFinder.class);
         Mockito.when(mapCell.getxMap()).thenReturn(xs);
         Mockito.when(mapCell.getyMap()).thenReturn(ys);
 
         Mockito.when(mF.getMaxima()).thenReturn(new Polygon(indexes, frames, 7));
 
-        protrusionVis.addPointsToImage();
+        protrusionVis.addMaximaToImage(mapCell, mF);
 
-        protrusionVis.originalImage.show();
-        while (protrusionVis.originalImage.isVisible()) {
+        protrusionVis.getOriginalImage().setTitle("testAddPointsToImage");
+        protrusionVis.getOriginalImage().show();
+        while (protrusionVis.getOriginalImage().isVisible()) {
             Thread.sleep(1500);
         }
+    }
+
+    /**
+     * Test method for {@link uk.ac.warwick.wsbc.QuimP.plugin.Prot_Analysis.ProtrusionVis#PolygonRoi2Map(java.util.List)}.
+     */
+    @Test
+    public void testPolygonRoi2Map() throws Exception {
+        List<Point2i> expected = new ArrayList<>();
+        expected.add(new Point2i(1, 11));
+        expected.add(new Point2i(1, 44));
+        int[] x1 = { 1, 2, 3, 1 };
+        int[] y1 = { 11, 22, 33, 44 };
+        int[] x2 = { 101, 102, 103 };
+        int[] y2 = { 111, 112, 113 };
+
+        ArrayList<PolygonRoi> p = new ArrayList<>();
+        p.add(new PolygonRoi(x1, y1, x1.length, Roi.FREELINE));
+        p.add(new PolygonRoi(x2, y2, x2.length, Roi.FREELINE));
+
+        @SuppressWarnings("unchecked")
+        List<Point2i> ret = (List<Point2i>) accessPrivate("PolygonRoi2Point2i", protrusionVis,
+                new Object[] { p }, new Class<?>[] { List.class });
+        List<Point2i> result = ret.stream().filter(e -> e.getX() == 1).collect(Collectors.toList());
+        assertThat(result, is(expected));
+
+    }
+
+    /**
+     * Test method for {@link uk.ac.warwick.wsbc.QuimP.plugin.Prot_Analysis.ProtrusionVis#addTrackingLinesToImage(uk.ac.warwick.wsbc.QuimP.STmap, java.util.List)}.
+     */
+    @Test
+    public void testAddTrackingLinesToImage() throws Exception {
+        ArrayList<PolygonRoi> testRoi = new ArrayList<>();
+        int[] frames = { 2, 3, 4, 5, 6, 7, 8 };
+        int[] indexes = { 0, 0, 0, 0, 0, 0, 0 };
+        double[][] xs = { { 0 }, { 0 }, { 10 }, { 11 }, { 12 }, { 13 }, { 14 }, { 15 }, { 16 } };
+        double[][] ys = { { 0 }, { 0 }, { 50 }, { 51 }, { 52 }, { 53 }, { 54 }, { 55 }, { 56 } };
+
+        STmap mapCell = Mockito.mock(STmap.class);
+        Mockito.when(mapCell.getxMap()).thenReturn(xs);
+        Mockito.when(mapCell.getyMap()).thenReturn(ys);
+
+        testRoi.add(new PolygonRoi(indexes, frames, 7, Roi.POLYLINE));
+        LOGGER.trace(Arrays.toString(testRoi.get(0).getPolygon().ypoints));
+        protrusionVis.addTrackingLinesToImage(mapCell, testRoi);
+
+        protrusionVis.getOriginalImage().setTitle("testAddTrackingLinesToImage");
+        protrusionVis.getOriginalImage().show();
+        while (protrusionVis.getOriginalImage().isVisible()) {
+            Thread.sleep(1500);
+        }
+    }
+
+    @Test
+    public void testListPoint2iComparator() {
+        List<Point2i> expected = new ArrayList<>();
+        expected.add(new Point2i(1, 1));
+        expected.add(new Point2i(1, 11));
+        expected.add(new Point2i(5, 44));
+
+        List<Point2i> result = new ArrayList<>();
+        result.add(new Point2i(1, 11));
+        result.add(new Point2i(5, 44));
+        result.add(new Point2i(1, 1));
+
+        Collections.sort(result, new ListPoint2iComparator());
+        assertThat(result, is(expected));
     }
 
 }
