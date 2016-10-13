7433d4d36c84dd3ca54d6bc77af72cb02682a1ee
Piotr Baniukiewicz <P.Baniukiewicz@warwick.ac.uk>
2016-10-13 15:23:21 +0100

    #220: Simple waiter
    Task-Url: http://www.trac-wsbc.linkpc.net:8080/trac/QuimP/ticket/220

--- src/main/java/uk/ac/warwick/wsbc/QuimP/registration/Registration.java
+++ src/main/java/uk/ac/warwick/wsbc/QuimP/registration/Registration.java
@@ -5,11 +5,14 @@ import java.awt.FlowLayout;
 import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingWorker;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
@@ -20,6 +23,8 @@ import javax.swing.event.ChangeListener;
 public class Registration extends JDialog implements ActionListener, ChangeListener {
 
     private JButton bOk, bCancel;
+    private JLabel lWait;
+    private boolean waited = false;
     /**
      * 
      */
@@ -52,6 +57,9 @@ public class Registration extends JDialog implements ActionListener, ChangeListe
         caButtons.add(bCancel);
         wndpanel.add(caButtons, BorderLayout.SOUTH);
 
+        lWait = new JLabel(" ");
+        wndpanel.add(lWait, BorderLayout.NORTH);
+
         add(wndpanel);
         pack();
         setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
@@ -59,28 +67,15 @@ public class Registration extends JDialog implements ActionListener, ChangeListe
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == bCancel) {
-            delay(5);
-            // dispose();
+        if (e.getSource() == bCancel && waited == false) {
+            bOk.setEnabled(false);
+            bCancel.setEnabled(false);
+            Worker w = new Worker(5);
+            w.execute();
         }
-
-    }
-
-    /**
-     * Wait <tt>i</tt> seconds before proceeding.
-     * @param i
-     */
-    private void delay(int i) {
-        int n = i;
-        do {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            n--;
-            bCancel.setText(Integer.toString(i - n) + " sec");
-        } while (n >= 0);
+        if (e.getSource() == bCancel && waited == true) {
+            dispose();
+        }
 
     }
 
@@ -89,6 +84,38 @@ public class Registration extends JDialog implements ActionListener, ChangeListe
         if (arg0.getSource() == bCancel) {
         }
 
+    }
+
+    /**
+     * 
+     * @author p.baniukiewicz
+     * @see http://stackoverflow.com/questions/11817688/how-to-update-swing-gui-from-inside-a-long-method
+     */
+    public class Worker extends SwingWorker<String, String> {
+        private int wait;
+
+        public Worker(int wait) {
+            this.wait = wait;
+        }
+
+        @Override
+        protected String doInBackground() throws Exception {
+            // This is what's called in the .execute method
+            for (int i = 0; i < wait; i++) {
+                // This sends the results to the .process method
+                publish(String.valueOf(wait - i));
+                Thread.sleep(1000);
+            }
+            waited = true;
+            bOk.setEnabled(true);
+            bCancel.setEnabled(true);
+            return null;
+        }
+
+        protected void process(List<String> item) {
+            // This updates the UI
+            lWait.setText(item.get(0));
+        }
     }
 
 }
