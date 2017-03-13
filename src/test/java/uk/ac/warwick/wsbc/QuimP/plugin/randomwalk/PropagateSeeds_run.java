1be98483b96ff10ca9e55c1b135c7b9555518aa2
Piotr Baniukiewicz <P.Baniukiewicz@warwick.ac.uk>
2017-03-13 13:58:32 +0000

    Bioinformatics

--- src/test/java/uk/ac/warwick/wsbc/QuimP/plugin/randomwalk/PropagateSeeds_run.java
+++ src/test/java/uk/ac/warwick/wsbc/QuimP/plugin/randomwalk/PropagateSeeds_run.java
@@ -1,5 +1,8 @@
 package uk.ac.warwick.wsbc.QuimP.plugin.randomwalk;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,6 +12,8 @@ import ij.ImageJ;
 import ij.ImagePlus;
 import ij.ImageStack;
 import ij.process.ImageProcessor;
+import uk.ac.warwick.wsbc.quimp.plugin.randomwalk.PropagateSeeds;
+import uk.ac.warwick.wsbc.quimp.plugin.randomwalk.RandomWalkException;
 
 /**
  * @author p.baniukiewicz
@@ -16,46 +21,59 @@ import ij.process.ImageProcessor;
  */
 public class PropagateSeeds_run {
 
-    static {
-        System.setProperty("logback.configurationFile", "quimp-logback.xml");
-    }
-    private static final Logger LOGGER =
-            LoggerFactory.getLogger(PropagateSeeds_run.class.getName());
+  static {
+    // System.setProperty("logback.configurationFile", "quimp-logback.xml");
+  }
+  private static final Logger LOGGER = LoggerFactory.getLogger(PropagateSeeds_run.class.getName());
 
-    /**
-     * @param args
-     * @throws RandomWalkException
-     */
-    public static void main(String[] args) throws RandomWalkException {
-        ImageJ ij = new ImageJ();
-        ImagePlus mask = IJ.openImage(
-                "/home/baniuk/baniuk1@gmail.com/Warwick/Abstract/C1-talA_mNeon_bleb_0pt7%agar_FLU_snakemask.tif");
-        ImagePlus org = IJ.openImage(
-                "/home/baniuk/baniuk1@gmail.com/Warwick/Abstract/C1-talA_mNeon_bleb_0pt7%agar_FLU.tif");
-        mask.show();
-        org.show();
-        ImageStack is = mask.getStack();
-        PropagateSeeds.Contour cc = new PropagateSeeds.Contour(true);
-        for (int i = 1; i <= is.getSize(); i++) {
-            cc.propagateSeed(mask.getStack().getProcessor(i), 20, 10);
-        }
-        CompositeImage ret = (CompositeImage) cc.getCompositeSeed(org);
-        ret.show();
-        ImageStack resultstack_red = new ImageStack(mask.getWidth(), mask.getHeight());
-        ImageStack resultstack_green = new ImageStack(mask.getWidth(), mask.getHeight());
-        for (int i = 1; i <= is.getSize(); i++) {
-            ret.setZ(i);
-            ImageProcessor red = ret.getProcessor(1);
-            ImageProcessor green = ret.getProcessor(2);
-            resultstack_red.addSlice(red);
-            resultstack_green.addSlice(green);
-        }
-        IJ.saveAsTiff(new ImagePlus("red", resultstack_red),
-                "/home/baniuk/baniuk1@gmail.com/Warwick/Abstract/rough_snakemask_shrink_stack.tif");
-        IJ.saveAsTiff(new ImagePlus("green", resultstack_green),
-                "/home/baniuk/baniuk1@gmail.com/Warwick/Abstract/rough_snakemask_expand_stack.tif");
+  /**
+   * @param args
+   * @throws RandomWalkException
+   */
+  public static void main(String[] args) throws RandomWalkException {
+    String folder =
+            "/home/baniuk/baniuk1@gmail.com/Warwick/BioinformaticsPaper/Piotr_test_data_src/TestSeg";
+    String casename = "RM1_C1-talA_GFP_rnd_motility_FLU";
 
-        ij.quit();
+    int sh = 14;
+    int ex = 10;
+
+    String filename = casename.substring(4);
+    Path orgimage = Paths.get(folder, casename, filename + ".tif");
+    Path maskimage = Paths.get(folder, casename, filename + "_snakemask.tif");
+    Path maskimageOutEx = Paths.get(folder, casename, filename + "_snakemask_expand.tif");
+    Path maskimageOutSh = Paths.get(folder, casename, filename + "_snakemask_shrink.tif");
+    Path compositeOut =
+            Paths.get(folder, casename, filename + "_composite_sh" + sh + "_ex" + ex + ".tif");
+
+    ImageJ ij = new ImageJ();
+    ImagePlus mask = IJ.openImage(maskimage.toString());
+    ImagePlus org = IJ.openImage(orgimage.toString());
+    mask.show();
+    org.show();
+
+    ImageStack is = mask.getStack();
+    PropagateSeeds.Contour cc = new PropagateSeeds.Contour(true);
+    for (int i = 1; i <= is.getSize(); i++) {
+      cc.propagateSeed(mask.getStack().getProcessor(i), sh, ex);
+      IJ.showProgress(i, is.getSize());
     }
+    CompositeImage ret = (CompositeImage) cc.getCompositeSeed(org);
+    ret.show();
+    ImageStack resultstack_red = new ImageStack(mask.getWidth(), mask.getHeight());
+    ImageStack resultstack_green = new ImageStack(mask.getWidth(), mask.getHeight());
+    for (int i = 1; i <= is.getSize(); i++) {
+      ret.setZ(i);
+      ImageProcessor red = ret.getProcessor(1);
+      ImageProcessor green = ret.getProcessor(2);
+      resultstack_red.addSlice(red);
+      resultstack_green.addSlice(green);
+    }
+    IJ.saveAsTiff(new ImagePlus("red", resultstack_red), maskimageOutSh.toString());
+    IJ.saveAsTiff(new ImagePlus("green", resultstack_green), maskimageOutEx.toString());
+    IJ.saveAsTiff(ret, compositeOut.toString());
+
+    // ij.quit();
+  }
 
 }
