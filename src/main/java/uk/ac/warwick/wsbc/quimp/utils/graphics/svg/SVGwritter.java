CellDynamics/QuimP

commit efbbdf74001de266634f29fe1981eb3ee6c19dfd

Massive refactoring

src/main/java/uk/ac/warwick/wsbc/quimp/utils/graphics/svg/SVGwritter.java
@@ -4,10 +4,10 @@ import java.awt.Rectangle;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 
+import ij.IJ;
 import uk.ac.warwick.wsbc.quimp.QColor;
 import uk.ac.warwick.wsbc.quimp.geom.ExtendedVector2d;
 
-// TODO: Auto-generated Javadoc
 /**
  * Plot shapes on SVG image.
  * 
@@ -19,9 +19,9 @@ public abstract class SVGwritter {
   /**
    * Generate typical header.
    * 
-   * @param osw
-   * @param d
-   * @throws IOException
+   * @param osw file to write
+   * @param d bounding box
+   * @throws IOException on file error
    */
   public static void writeHeader(OutputStreamWriter osw, Rectangle d) throws IOException {
     Rectangle d1 = new Rectangle(d);
@@ -39,12 +39,20 @@ public abstract class SVGwritter {
   }
 
   /**
+   * Write to file svg definition of frawn object.
+   * 
+   * @param osw file to write
+   * @throws IOException on file error
+   */
+  public abstract void draw(OutputStreamWriter osw) throws IOException;
+
+  /**
    * Represent circle on SVG image.
    * 
    * @author p.baniukiewicz
    *
    */
-  static public class Qcircle extends SVGwritter {
+  public static class Qcircle extends SVGwritter {
 
     /**
      * x coordinate of center.
@@ -61,9 +69,7 @@ public abstract class SVGwritter {
     public double radius;
 
     /**
-     * The colour.
-     * 
-     * if null object has no filling
+     * The colour. If null object has no filling
      */
     public QColor colour;
 
@@ -93,23 +99,24 @@ public abstract class SVGwritter {
       thickness = 0.0;
     }
 
-    /**
-     * Draw.
-     *
-     * @param osw the osw
-     * @throws IOException Signals that an I/O exception has occurred.
+    /*
+     * (non-Javadoc)
+     * 
+     * @see uk.ac.warwick.wsbc.quimp.utils.graphics.svg.SVGwritter#draw(java.io.OutputStreamWriter)
      */
+    @Override
     public void draw(OutputStreamWriter osw) throws IOException {
       String col; // fill colour
-      if (colour == null)
+      if (colour == null) {
         col = "none";
-      else
+      } else {
         col = colour.getColorSVG();
-      //!<
-      osw.write("<circle cx=\"" + x1 + "\"" + " cy=\"" + y1 + "\"" + " r=\"" + radius + "\""
+      }
+      //!>
+      osw.write("<circle cx=\"" + x1 + "\"" + " cy=\"" + y1 + "\"" + " rect=\"" + radius + "\""
               + " fill=\"" + col + "\"" + " stroke=\"" + strokecolour.getColorSVG() + "\""
               + " stroke-width=\"" + thickness + "\"/>\n");
-      /**/
+      //!<
     }
   }
 
@@ -175,12 +182,12 @@ public abstract class SVGwritter {
       return Math.sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
     }
 
-    /**
-     * Draw.
-     *
-     * @param osw the osw
-     * @throws IOException Signals that an I/O exception has occurred.
+    /*
+     * (non-Javadoc)
+     * 
+     * @see uk.ac.warwick.wsbc.quimp.utils.graphics.svg.SVGwritter#draw(java.io.OutputStreamWriter)
      */
+    @Override
     public void draw(OutputStreamWriter osw) throws IOException {
       osw.write("<line x1=\"" + x1 + "\" y1=\"" + y1 + "\" x2=\"" + x2 + "\" y2=\"" + y2 + "\" ");
       osw.write("style=\"stroke:" + colour.getColorSVG() + ";stroke-width:" + thickness + "\"/>\n");
@@ -216,16 +223,23 @@ public abstract class SVGwritter {
     public String font;
 
     /**
+     * Position of text.
+     */
+    public ExtendedVector2d pos;
+
+    /**
      * Instantiates a new qtext.
      *
      * @param t the t
      * @param s the s
      * @param f the f
+     * @param pos position
      */
-    public Qtext(String t, double s, String f) {
+    public Qtext(String t, double s, String f, ExtendedVector2d pos) {
       text = t;
       size = s;
       font = f;
+      this.pos = pos;
       colour = new QColor(0, 0, 0);
     }
 
@@ -238,27 +252,28 @@ public abstract class SVGwritter {
       return text.length();
     }
 
-    /**
-     * Draw.
-     *
-     * @param osw the osw
-     * @param l the l
-     * @throws IOException Signals that an I/O exception has occurred.
+    /*
+     * (non-Javadoc)
+     * 
+     * @see uk.ac.warwick.wsbc.quimp.utils.graphics.svg.SVGwritter#draw(java.io.OutputStreamWriter)
      */
-    public void draw(OutputStreamWriter osw, ExtendedVector2d l) throws IOException {
-      osw.write("\n<text x=\"" + l.getX() + "\" y=\"" + l.getY() + "\" " + "style=\"font-family: "
-              + font + ";font-size: " + size + ";fill: " + colour.getColorSVG() + "\">" + text
-              + "</text>");
+    @Override
+    public void draw(OutputStreamWriter osw) throws IOException {
+      osw.write("\n<text x=\"" + pos.getX() + "\" y=\"" + pos.getY() + "\" "
+              + "style=\"font-family: " + font + ";font-size: " + size + ";fill: "
+              + colour.getColorSVG() + "\">" + text + "</text>");
+
     }
   }
 
   /**
+   * Create axes in polar coordinates.
    * 
    * @author p.baniukiewicz
    *
    */
   public static class QPolarAxes extends SVGwritter {
-    private Rectangle r;
+    private Rectangle rect;
 
     /**
      * The colour.
@@ -271,26 +286,27 @@ public abstract class SVGwritter {
     public double thickness;
 
     /**
+     * Create polar axes.
      * 
-     * @param r
+     * @param rect boundaries
      */
-    public QPolarAxes(Rectangle r) {
-      this.r = r;
+    public QPolarAxes(Rectangle rect) {
+      this.rect = rect;
       colour = new QColor(0.5, 0.5, 0.5);
       thickness = 0.01;
     }
 
-    /**
-     * Draw.
-     *
-     * @param osw the osw
-     * @throws IOException Signals that an I/O exception has occurred.
+    /*
+     * (non-Javadoc)
+     * 
+     * @see uk.ac.warwick.wsbc.quimp.utils.graphics.svg.SVGwritter#draw(java.io.OutputStreamWriter)
      */
+    @Override
     public void draw(OutputStreamWriter osw) throws IOException {
       // plot parameters
-      double x0 = r.getLocation().getX() + r.getWidth() / 2;
-      double y0 = r.getLocation().getY() + r.getHeight() / 2;
-      double radius = r.getHeight() / 2;
+      double x0 = rect.getLocation().getX() + rect.getWidth() / 2;
+      double y0 = rect.getLocation().getY() + rect.getHeight() / 2;
+      double radius = rect.getHeight() / 2;
       // main circle
       Qcircle qcmain = new Qcircle(x0, y0, radius);
       qcmain.colour = new QColor(1, 1, 1);
@@ -322,11 +338,103 @@ public abstract class SVGwritter {
       } while (++i < n);
 
       // middle
-      Qcircle qcm = new Qcircle(r.getLocation().getX() + r.getWidth() / 2,
-              r.getLocation().getY() + r.getHeight() / 2, thickness * 4);
+      Qcircle qcm = new Qcircle(rect.getLocation().getX() + rect.getWidth() / 2,
+              rect.getLocation().getY() + rect.getHeight() / 2, thickness * 4);
       qcm.colour = colour;
       qcm.draw(osw);
 
     }
+  }
+
+  /**
+   * Plot scale bar on svg.
+   * 
+   * @author rtyson
+   *
+   */
+  public static class QScaleBar extends SVGwritter {
+
+    private double length;
+    private String units;
+    private int value;
+    /**
+     * Line thickness.
+     */
+    public double thickness;
+    /**
+     * Text colour.
+     */
+    public QColor colour;
+    private ExtendedVector2d location;
+    private SVGwritter.Qtext text;
+
+    /**
+     * Constructor.
+     * 
+     * @param l position of text
+     * @param u units
+     * @param v value
+     * @param s scale
+     */
+    public QScaleBar(ExtendedVector2d l, String u, int v, double s) {
+      location = l;
+      units = u;
+      value = v;
+      thickness = 1;
+      colour = new QColor(1, 1, 1);
+      this.setScale(s);
+      text = new SVGwritter.Qtext(IJ.d2s(value, 0) + units, 6, "Courier", l);
+      text.colour = colour;
+    }
+
+    /**
+     * Set scale.
+     * 
+     * @param s scale
+     */
+    public void setScale(double s) {
+      length = value / s;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see uk.ac.warwick.wsbc.quimp.utils.graphics.svg.SVGwritter#draw(java.io.OutputStreamWriter)
+     */
+    @Override
+    public void draw(OutputStreamWriter osw) throws IOException {
+      SVGwritter.Qline body;
+
+      double tickSize = 2 * thickness;
+
+      ExtendedVector2d end = new ExtendedVector2d(location.getX(), location.getY());
+      end.addVec(new ExtendedVector2d(length, 0));
+      body = new SVGwritter.Qline(location.getX(), location.getY(), end.getX(), end.getY());
+      body.thickness = thickness;
+      body.colour = colour;
+
+      SVGwritter.Qline ltick;
+      SVGwritter.Qline rtick;
+      ltick = new SVGwritter.Qline(location.getX(), location.getY() + tickSize, location.getX(),
+              location.getY() - tickSize);
+      rtick = new SVGwritter.Qline(end.getX(), end.getY() + tickSize, end.getX(),
+              end.getY() - tickSize);
+      ltick.thickness = thickness;
+      ltick.colour = colour;
+      rtick.thickness = thickness;
+      rtick.colour = colour;
+
+      ltick.draw(osw);
+      rtick.draw(osw);
+      body.draw(osw);
+
+      // centre the text
+      int textLength = 2 + Integer.toString(value).length();
+      textLength = textLength * 4;
+      double textDis = (body.length() - textLength) / 2;
+      text.pos = new ExtendedVector2d(location.getX() + textDis, location.getY() - 2);
+      text.draw(osw);
+    }
+
   }
 }

src/main/java/uk/ac/warwick/wsbc/quimp/plugin/ParamList.java
@@ -1,5 +1,3 @@
-/**
- */
 package uk.ac.warwick.wsbc.quimp.plugin;
 
 import java.util.Map;
@@ -7,9 +5,8 @@ import java.util.Map;
 import uk.ac.warwick.wsbc.quimp.plugin.utils.LinkedStringMap;
 import uk.ac.warwick.wsbc.quimp.plugin.utils.StringParser;
 
-// TODO: Auto-generated Javadoc
 /**
- * List of parameters in <key,value> HashList, where both key and value are java.lang.String and key
+ * List of parameters in [key,value] HashList, where both key and value are java.lang.String and key
  * is case insensitive.
  * 
  * @author p.baniukiewicz
@@ -33,7 +30,7 @@ public class ParamList extends LinkedStringMap<String> {
   }
 
   /**
-   * Default constructor
+   * Default constructor.
    */
   public ParamList() {
     super();
@@ -82,7 +79,7 @@ public class ParamList extends LinkedStringMap<String> {
   /**
    * Get Integer value from list associated with \c key. Key is not case sensitive.
    * 
-   * For safety it take double and then converts it to integer. It helps with dealing with type
+   * <p>For safety it take double and then converts it to integer. It helps with dealing with type
    * changing in QWindowBuilder. By default it keeps all in double and passed data returns in
    * double as well
    * 
@@ -126,7 +123,7 @@ public class ParamList extends LinkedStringMap<String> {
   /**
    * Get string associated with key and parse it to split according to delimiter.
    * 
-   * May be used only when val under key can be parsed
+   * <p>May be used only when val under key can be parsed
    * 
    * @param key to be read
    * @return Split substrings or empty array in case of any error

src/main/java/uk/ac/warwick/wsbc/quimp/plugin/randomwalk/RandomWalkSegmentation.java
@@ -131,7 +131,7 @@ public class RandomWalkSegmentation {
    * 
    * @param ip image to segment
    * @param params parameters
-   * @throws RandomWalkException
+   * @throws RandomWalkException on wrong image format
    */
   public RandomWalkSegmentation(ImageProcessor ip, Params params) throws RandomWalkException {
     if (ip.getBitDepth() != 8 && ip.getBitDepth() != 16) {
@@ -1156,12 +1156,11 @@ public class RandomWalkSegmentation {
   /**
    * Add in-place this matrix to another.
    * 
+   * <p>src/test/resources/Matlab/rw_laplace4_java_base.m This is source file of segmentation
+   * in Matlab that was a base for RandomWalkSegmentation Implementation
+   * 
    * @author p.baniukiewicz
    *
-   */
-  /**
-   * @example src/test/resources/Matlab/rw_laplace4_java_base.m This is source file of segmentation
-   *          in Matlab that was a base for RandomWalkSegmentation Implementation
    */
   static class MatrixDotAdd implements RealMatrixChangingVisitor {
 
@@ -1245,11 +1244,6 @@ public class RandomWalkSegmentation {
     }
 
     @Override
-    /**
-     * @example src/test/resources/Matlab/rw_laplace4_java_base.m This is source file of
-     *          segmentation in
-     *          Matlab that was a base for RandomWalkSegmentation Implementation
-     */
     public double end() {
       return 0;
     }

src/main/java/uk/ac/warwick/wsbc/quimp/plugin/utils/DataLoader.java
@@ -14,24 +14,31 @@ import org.slf4j.LoggerFactory;
 
 import ij.process.FloatPolygon;
 
-// TODO: Auto-generated Javadoc
 /**
  * Simple data loader for test.
  * 
- * Load contours saved as one-column list with interleaving coordinates of vertices:
+ * <p>Load contours saved as one-column list with interleaving coordinates of vertices:
  * 
- * The file must contain even number of data. Exemplary code in Matlab to create such file:
+ * <p>The file must contain even number of data. Exemplary code in Matlab to create such file:
  * 
  * <pre>
  * <code>
  * X1 Y1 X2 Y2 ... Xn Yn
  * 
- * addpath('/home/p.baniukiewicz/Documents/QuimP11_MATLAB/') qCells =
- *           readQanalysis('Resources/after-macro'); testFrames = [75 125 137 1]; clear coords; for
- *           i=1:length(testFrames) coords{i} = qCells.outlines{testFrames(i)}(:,2:3); end for
- *           i=1:length(testFrames) fid = fopen(['testData_' num2str(testFrames(i)) '.dat'], 'w');
- *           xy = coords{i}; xyr = reshape(xy',[],1); % x first fprintf(fid,'%.4f\n',xyr);
- *           fclose(fid); end
+ * addpath('/home/p.baniukiewicz/Documents/QuimP11_MATLAB/')
+ * qCells = readQanalysis('Resources/after-macro');
+ * testFrames = [75 125 137 1];
+ * clear coords;
+ * for i=1:length(testFrames)
+ *     coords{i} = qCells.outlines{testFrames(i)}(:,2:3);
+ * end
+ * for i=1:length(testFrames)
+ *     fid = fopen(['testData_' num2str(testFrames(i)) '.dat'], 'w');
+ *     xy = coords{i};
+ *     xyr = reshape(xy',[],1); % x first
+ *     fprintf(fid,'%.4f\n',xyr);
+ *     fclose(fid);
+ * end
  * </code>
  * </pre>
  * 
@@ -46,14 +53,14 @@ public class DataLoader {
   static final Logger LOGGER = LoggerFactory.getLogger(DataLoader.class.getName());
   private List<Double> data;
   /**
-   * 
+   * Data loaded on constructor call.
    */
-  public List<Point2d> Vert;
+  public List<Point2d> vert;
 
   /**
    * Construct dataLoader object.
    * 
-   * Open and read datafile
+   * <p>Open and read datafile
    * 
    * @param fileName file with data (with path)
    * @throws FileNotFoundException on bad file
@@ -61,42 +68,44 @@ public class DataLoader {
    */
   public DataLoader(String fileName) throws FileNotFoundException, IllegalArgumentException {
     data = new ArrayList<Double>();
-    Vert = new ArrayList<Point2d>();
+    vert = new ArrayList<Point2d>();
     Scanner scanner = new Scanner(new File(fileName));
     scanner.useLocale(Locale.US);
-    while (scanner.hasNextDouble())
+    while (scanner.hasNextDouble()) {
       data.add(scanner.nextDouble());
+    }
     scanner.close();
     convertToPoint2d();
     LOGGER.debug("File: " + fileName + " loaded");
   }
 
   /**
-   * Convert read List<Double> to List<Point2d>
+   * Convert read List/<Double/> to List/<Point2d/>.
    * 
-   * @throws IllegalArgumentException
+   * @throws IllegalArgumentException on wrong number of data in file
    */
   private void convertToPoint2d() throws IllegalArgumentException {
-    if (data.size() % 2 != 0)
+    if (data.size() % 2 != 0) {
       throw new IllegalArgumentException("Data must be multiply of 2");
+    }
     ListIterator<Double> it = data.listIterator();
     while (it.hasNext()) {
-      Vert.add(new Point2d(it.next().doubleValue(), // x coord
+      vert.add(new Point2d(it.next().doubleValue(), // x coord
               it.next().doubleValue())); // y coord from input file
     }
   }
 
   /**
-   * Return loaded data
+   * Return loaded data.
    * 
-   * @return loaded polygon as List<Point2d>
+   * @return loaded polygon as List/<Point2d/>
    */
   public List<Point2d> getData() {
-    return Vert;
+    return vert;
   }
 
   /**
-   * Return loaded data as FloatPolygon
+   * Return loaded data as FloatPolygon.
    * 
    * @return Loaded polygon as FloatPolygon
    */
@@ -106,11 +115,11 @@ public class DataLoader {
   }
 
   /**
-   * Convert loaded data to string
+   * Convert loaded data to string.
    * 
    * @return String representation of loaded data
    */
   public String toString() {
-    return Vert.toString();
+    return vert.toString();
   }
 }

src/main/java/uk/ac/warwick/wsbc/quimp/plugin/utils/IPadArray.java
@@ -1,32 +1,31 @@
 package uk.ac.warwick.wsbc.quimp.plugin.utils;
 
-// TODO: Auto-generated Javadoc
 /**
  * Basic interface for all family of filters based on moving window.
  * 
- * Supports various methods of data padding
+ * <p>Supports various methods of data padding
  * 
  * @author p.baniukiewicz
  *
  */
 public interface IPadArray {
   /**
-   * defines circular padding
+   * defines circular padding.
    */
   int CIRCULARPAD = 1;
   /**
-   * defines symmetric padding
+   * defines symmetric padding.
    */
   int SYMMETRICPAD = 2;
 
   /**
    * Helper method to pick values from X, Y arrays.
    * 
-   * It accepts negative indexes as well as larger than X.size() and does in-place padding.
+   * <p>It accepts negative indexes as well as larger than X.size() and does in-place padding.
    * Returns new proper index for array that accounts padding e.g. for input = -2 it returns
    * last+2 if padding is circular
    * 
-   * Do no check relations of window (provided \c index) to whole data size. May be unstable for
+   * <p>Do no check relations of window (provided \c index) to whole data size. May be unstable for
    * certain cases.
    * 
    * @param dataLength Length of data
@@ -40,17 +39,20 @@ public interface IPadArray {
 
     switch (mode) {
       case CIRCULARPAD:
-        if (index < 0)
-          return (dataLength + index); // for -1 points last element
-        if (index >= dataLength)
-          return (index - dataLength); // for after last points
-                                       // to first
+        if (index < 0) {
+          return (dataLength + index);
+        } // for -1 points last element
+        if (index >= dataLength) {
+          return (index - dataLength);
+        } // for after last points to first
         break;
       case SYMMETRICPAD:
-        if (index < 0)
+        if (index < 0) {
           return -index - 1;
-        if (index >= dataLength)
+        }
+        if (index >= dataLength) {
           return (dataLength - (index - dataLength) - 1);
+        }
         break;
       default:
         throw new IllegalArgumentException("Padding mode not supported");

commit c4def2b3aecef0c8ade6d1718d8cf967002b0d53

Fixed bug in plotting svg circles
Introduced in efbbdf7

src/main/java/uk/ac/warwick/wsbc/quimp/utils/graphics/svg/SVGwritter.java
@@ -113,7 +113,7 @@ public abstract class SVGwritter {
         col = colour.getColorSVG();
       }
       //!>
-      osw.write("<circle cx=\"" + x1 + "\"" + " cy=\"" + y1 + "\"" + " rect=\"" + radius + "\""
+      osw.write("<circle cx=\"" + x1 + "\"" + " cy=\"" + y1 + "\"" + " r=\"" + radius + "\""
               + " fill=\"" + col + "\"" + " stroke=\"" + strokecolour.getColorSVG() + "\""
               + " stroke-width=\"" + thickness + "\"/>\n");
       //!<